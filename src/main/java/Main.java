a05428991eb2ea90c59c3f1ee1fd5ca43181f410
Kuzmin Ilya <i.kyzmin.v@gmail.com>
2017-04-11 10:49:12 +0400

    simple akka example

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
+import actor.Generator;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+
 /**
  * Created by Илья on 11.04.2017.
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static String ACTOR_SYSTEM_NAME = "PrimeNumberSystem";
+    public static String GENERATOR = "generator";
 
+    public static void main(String[] args) {
+        ActorSystem actorSystem = ActorSystem.create(ACTOR_SYSTEM_NAME);
+        ActorRef generator = actorSystem.actorOf(Generator.props(), GENERATOR);
+        generator.tell("Hello", ActorRef.noSender());
+        actorSystem.terminate();
     }
 }
--- src/main/java/actor/Generator.java
+++ src/main/java/actor/Generator.java
@@ -1,6 +1,8 @@
 package actor;
 
 import akka.actor.AbstractActor;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 import akka.japi.pf.ReceiveBuilder;
@@ -8,16 +10,22 @@ import akka.japi.pf.ReceiveBuilder;
 /**
  * Created by Илья on 11.04.2017.
  */
-public class Generator extends AbstractActor {
+public class Generator extends UntypedActor {
+
+    public static Props props() {
+        return Props.create(Generator.class, Generator::new);
+    }
 
     private final LoggingAdapter log = Logging.getLogger(context().system(), this);
 
+    @Override
+    public void onReceive(Object o) throws Throwable {
+        if (o instanceof String) {
+            System.out.println(o);
+        }
+    }
+
     public Generator() {
-        receive(ReceiveBuilder.
-                match(String.class, s -> {
-                    log.info("Received String message: {}", s);
-                }).
-                matchAny(o -> log.info("received unknown message")).build()
-        );
+
     }
 }
